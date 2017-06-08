834083a49ca5bd7bacbf8df115cabb32ddaee69f
Lei Zhao <zhao434@usc.edu>
2017-06-08 08:00:05 -0700

    2017-06-08 08:00:03 auto check in

---
+++ FavoriteQuotes/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { FavoritesPageModule } from '../pages/favorites/favorites.module';
+import { QuotesPageModule } from '../pages/quotes/quotes.module';
+import { SettingsPageModule } from '../pages/settings/settings.module';
+import { QuotePageModule } from '../pages/quote/quote.module';
+import { TabsPageModule } from '../pages/tabs/tabs.module';
+import { LibraryPageModule } from '../pages/library/library.module';
+
+@NgModule({
+  declarations: [
+    MyApp,
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    FavoritesPageModule,
+    LibraryPageModule,
+    QuotesPageModule,
+    QuotePageModule,
+    SettingsPageModule,
+    TabsPageModule,
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
+  ]
+})
+export class AppModule { }
---
+++ FavoriteQuotes/src/pages/library/library.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Quote } from '../../data/quote.interface';
+import quotes from '../../data/quotes';
+
+@IonicPage()
+@Component({
+  selector: 'page-library',
+  templateUrl: 'library.html',
+})
+export class LibraryPage {
+
+  quoteCollection: { category: string, quotes: Quote[], icon: string }[];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.quoteCollection = quotes;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LibraryPage');
+  }
+
+}
